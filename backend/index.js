076a2d46d3de9aa0667351727e7c839ca557ec31
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2024-02-01 16:21:15 +0100

    start back

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,49 @@
-// Load environment variables from .env file
-require("dotenv").config();
+// ./server/index.js
 
-// Import the Express application from src/app.js
-const app = require("./src/app");
+// Il me faut express
+const express = require("express");
+// Ici, le paquet cors me permet de gérer les CORS
+// https://developer.mozilla.org/fr/docs/Web/HTTP/CORS
+const cors = require("cors");
+const database = require("./database/database");
 
-// Get the port from the environment variables
-const port = process.env.APP_PORT;
+// J'initialise mon app
+const app = express();
 
-// Start the server and listen on the specified port
-app
-  .listen(port, () => {
-    console.info(`Server is listening on port ${port}`);
-  })
-  .on("error", (err) => {
-    console.error("Error:", err.message);
-  });
+const PORT = 3310;
+
+// J'utilise cors
+app.use(cors());
+
+// Je défini une route
+
+/**
+ * GET /
+ */
+app.get("/", async (req, res) => {
+  const SQL = "SELECT * FROM user";
+  try {
+    const users = await database.query(SQL);
+    res.json(users[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello World from post");
+});
+
+app.put("/", (req, res) => {
+  res.send("Hello World from put");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Hello World from delete");
+});
+
+// Puis je lance mon serveur
+app.listen(PORT, () => {
+  console.info(`Server started on port ${PORT}`);
+});
