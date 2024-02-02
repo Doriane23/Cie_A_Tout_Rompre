f574533f203a2d89e7dcb1eba5ae244de014f6c9
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2024-02-02 09:31:32 +0100

    Start Middleware

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,6 @@
-const express = require("express");
+const app = require("./src/app");
 
-const app = express();
 const PORT = 3310;
-
-const userControllers = require("./src/controllers/userController");
-const sayHelloMiddleware = require("./src/services/sayHello");
-
-app.post("/users", userControllers.add);
-app.get("/users", sayHelloMiddleware, userControllers.browse);
-app.get("/users/:id", userControllers.read);
-app.put("/users/:id", userControllers.edit);
-app.delete("/users/:id", userControllers.destroy);
 
 app.listen(PORT, () => {
   console.info(`Example app listening on PORT http://localhost:${PORT}`);
